9c52f57e9797d485bc6bbf7ed8e76d44b98d71f5
Anthony Ford <me@Anthonys-MacBook-Air.local>
2017-09-17 21:14:05 +0100

    and again

--- Number System/ViewController.swift
+++ Number System/ViewController.swift
@@ -1,24 +1,29 @@
-//
-//  ViewController.swift
-//  Number System
-//
-//  Created by Anthony Ford on 11/09/2017.
-//  Copyright © 2017 Anthony Ford. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
